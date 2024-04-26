1668dc9dbc3beb83af27256a892f2c1297e21d99
Yashwantttt <166321373+Yashwantttt@users.noreply.github.com>
2024-04-26 22:16:44 +0530

    Final Gameplay and Winner Declaration

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 confirm("You will play your game in console window")
-confirm("Press F12 to open console window")
-
+confirm("Type 2 in the 'Are You Ready' , Press F12 to open console window and refresh the page")
 console.log("Rock Paper & Scissors ")
 
 function getComputerChoice(){
@@ -44,19 +43,44 @@ function playRound(humanChoice,computerChoice){
     if ((humanChoice===1 && computerChoice===2) || (humanChoice===3 && computerChoice===1) || (humanChoice===2 && computerChoice===3) ){
         
         return("Computer wins this round");
+        computerScore ++;
     }
     else if((humanChoice===1 && computerChoice===3) || (humanChoice===2 & computerChoice===1) || (humanChoice===3 && computerChoice===2)){
         return("You won this round");
+        humanScore++;
     }
     else{
         return("Its a Draw !!")
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-console.log("You : ",move(humanSelection))
-console.log("Computer :",move(computerSelection))
-console.log(playRound(humanSelection, computerSelection))
+let playchoice= prompt("Are You Ready to Play? 1. Yes  2. No")
+const playy = Number(playchoice)
+if (playy===1){
+    let i = 1
+    while (i <=5) {
+        console.log("----Round ",i)
+        alert("Round "+i)
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        console.log("You : ",move(humanSelection))
+        console.log("Computer :",move(computerSelection))
+        console.log(playRound(humanSelection, computerSelection))
+        
+        i = i +1
+    }
+
+    if (humanScore>computerScore){
+        alert("You won ...You :",humanScore," win & computer :",computerScore," win")
+    }else if (humanScore<computerScore){
+        alert("Computer won ...You :",humanScore," wins & computer :",computerScore," wins")
+    }else{
+        alert("Its a draw")
+}
+
+}else{
+    
+}
 
