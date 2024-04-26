97d332e3c868b5cca8b7d68d34a8a26f9e00fb53
Yashwantttt <166321373+Yashwantttt@users.noreply.github.com>
2024-04-26 21:39:45 +0530

    Adding basic functionalities like deciding winner

--- js/script.js
+++ js/script.js
@@ -10,9 +10,10 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     x=prompt("Enter Your Choice    1. Rock🪨   2.Paper📄   3.Scissor✂️")
-    return x
+    return Number(x)
 }
 
+// Converts Number to their respective element
 function move(x){
     if (x===1){
         return('rock')
@@ -20,10 +21,42 @@ function move(x){
     else if(x===2){
         return('paper')
     }
-    else{
+    else if (x===3){
         return('scissor')
+    }
+    else{
+        console.log("Please enter valid option")
+    }
+    
+}
+// console.log(move(getComputerChoice()))
+// console.log(move(getHumanChoice()))
+
+// SCORE VARIABLES     
+let humanScore = 0;
+let computerScore=0;
+
+
+
+//  Game Logic
+function playRound(humanChoice,computerChoice){
+    
+    if ((humanChoice===1 && computerChoice===2) || (humanChoice===3 && computerChoice===1) || (humanChoice===2 && computerChoice===3) ){
+        
+        return("Computer wins this round");
+    }
+    else if((humanChoice===1 && computerChoice===3) || (humanChoice===2 & computerChoice===1) || (humanChoice===3 && computerChoice===2)){
+        return("You won this round");
+    }
+    else{
+        return("Its a Draw !!")
     }
 }
 
-console.log(move(getComputerChoice()))
-console.log(move(getHumanChoice()))
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log("You : ",move(humanSelection))
+console.log("Computer :",move(computerSelection))
+console.log(playRound(humanSelection, computerSelection))
+
