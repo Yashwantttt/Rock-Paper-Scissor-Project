eeca396b0510cb6defef18937a73afeeebaf552c
Yashwantttt <166321373+Yashwantttt@users.noreply.github.com>
2024-05-17 16:24:28 +0530

    Final Interface except counting of rounds

--- js/script.js
+++ js/script.js
@@ -1,12 +1,64 @@
+let humanScore = 0 ;
+let computerScore = 0 ;
 
-// confirm("You will play your game in console window")
-// confirm("Type 2 in the 'Are You Ready' , Press F12 to open console window and refresh the page")
-// console.log("Rock Paper & Scissors ")
+function getComputerChoice(){
+    const move = Math.floor(Math.random()*3+1)
+    return move
+}
 
-// function getComputerChoice(){
-//     const move = Math.floor(Math.random()*3+1)
-//     return move
-// }
+function move(x){
+    y=getComputerChoice();
+    imageDisplayer(y);
+    scoreCounter(humanScore,computerScore);
+    setTimeout(playRound,500,x,y);
+    
+}
+
+function playRound(humanChoice,computerChoice){
+    
+    if ((humanChoice===1 && computerChoice===2) || (humanChoice===3 && computerChoice===1) || (humanChoice===2 && computerChoice===3) ){
+        
+        alert("Computer wins this round");
+        computerScore ++;
+    }
+    else if((humanChoice===1 && computerChoice===3) || (humanChoice===2 & computerChoice===1) || (humanChoice===3 && computerChoice===2)){
+        alert("You won this round");
+        humanScore++;
+    }
+    else{
+        alert("Its a Draw !!")
+    }
+}
+
+function imageDisplayer(number){
+    let frame = document.getElementById('frame')
+    if (number===1){
+        frame.innerHTML='<img src="rock.png" alt="rock" class="choiceImg"> ';
+    }
+    else if (number===2){
+        frame.innerHTML='<img src="paper.png" alt="paper" class="choiceImg">';
+    }
+    else if (number===3){
+        frame.innerHTML='<img src="scissors.webp" alt="scissor" class="choiceImg">';
+    }
+}
+
+
+function scoreCounter(x,y){
+    document.getElementById("humanScore").textContent=x;
+    document.getElementById("computerScore").textContent=y;
+
+}
+
+
+
+
+
+
+
+
+
+
 
 // function getHumanChoice(){
 //     x=prompt("Enter Your Choice    1. Rock🪨   2.Paper📄   3.Scissor✂️")
@@ -14,23 +66,8 @@
 // }
 
 // // Converts Number to their respective element
-// function move(x){
-//     if (x===1){
-//         return('rock')
-//     }
-//     else if(x===2){
-//         return('paper')
-//     }
-//     else if (x===3){
-//         return('scissor')
-//     }
-//     else{
-//         console.log("Please enter valid option")
-//     }
-    
+
 // }
-// // console.log(move(getComputerChoice()))
-// // console.log(move(getHumanChoice()))
 
 // // SCORE VARIABLES     
 // let humanScore = 0;
@@ -39,21 +76,7 @@
 
 
 // //  Game Logic
-// function playRound(humanChoice,computerChoice){
-    
-//     if ((humanChoice===1 && computerChoice===2) || (humanChoice===3 && computerChoice===1) || (humanChoice===2 && computerChoice===3) ){
-        
-//         return("Computer wins this round");
-//         computerScore ++;
-//     }
-//     else if((humanChoice===1 && computerChoice===3) || (humanChoice===2 & computerChoice===1) || (humanChoice===3 && computerChoice===2)){
-//         return("You won this round");
-//         humanScore++;
-//     }
-//     else{
-//         return("Its a Draw !!")
-//     }
-// }
+
 
 
 // let playchoice= prompt("Are You Ready to Play? 1. Yes  2. No")
